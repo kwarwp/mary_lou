ac98465f746eaa64be5f3b46d4a9ac6a4a2d96c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:06:04 -0300

    Created sara/__log__.py at Timestamp: 2019-09-21 15:06:04

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Sep 21 2019 12:06:02.278 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  cenaobi-wankenibi = https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAGzRfe.img?h=564&w=624&m=6&q=60&o=f&l=f&x=246&y=140
+                            ^
+SyntaxError: invalid syntax
+'''},
