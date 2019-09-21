f297e18cac7f53eaed99ef1a300ff9a2b18c3691
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:13:21 -0300

    Saved roxanne/main.py at Timestamp: 2019-09-21 15:13:21

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,2 +1,3 @@
 # andre
-from _spy.vittolino.main import Cena, Elemento, Texto 
+from _spy.vittolino.main import Cena, Elemento, Texto
+cenapalacio = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Palacio_Fronteira_-_Lisbon_%287721187114%29.jpg/320px-Palacio_Fronteira_-_Lisbon_%287721187114%29.jpg"
