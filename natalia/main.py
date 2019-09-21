e7a374e3697091e09c2f9734492fe7da0133ef1a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:22:42 -0300

    Saved natalia/main.py at Timestamp: 2019-09-21 15:22:42

--- natalia/main.py
+++ natalia/main.py
@@ -1,1 +1,3 @@
 # Randolph
+from_spy.vittolino.main import Cena, Elemento, Texto
+cenaDigitalplatform = "https://www.osaogoncalo.com.br/geral/58944/plataforma-digital-completa-um-ano-na-engenhoca-e-a-comemoracao-sera-dia-30
