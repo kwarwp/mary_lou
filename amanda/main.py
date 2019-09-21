441ef0afec295fc398f67148994a1206888fbc0f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:21:55 -0300

    Saved amanda/main.py at Timestamp: 2019-09-21 15:21:55

--- amanda/main.py
+++ amanda/main.py
@@ -1,4 +1,4 @@
 # Thaise
 from_spy.vittolino.main import Cena, Elemento, Texto
 cenacastle = "https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzLzQ0OTA3MzQwNTE2NGZlYzRmY18yNjIyMjAyOV81NjM5MjI2MDA2MjkzMDJfNTIzMjMzODkyMTg5NDk2OTM0NF9uLmpwZyJdLFsicCIsInRodW1iIiwiMTIwMHg-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ/26222029_563922600629302_5232338921894969344_n.jpg"
-Elemento = "https://vignette.wikia.nocookie.net/o-segredo-alem-do-jardim/images/7/73/FrogJason1.png/revision/latest?cb=20150724234407&path-prefix=pt-br"
+elemento = "https://cdn.pixabay.com/photo/2015/10/08/05/18/fantasy-977198_960_720.png"
