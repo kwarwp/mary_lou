304a05502443deba0d41cc2933836514605ecfbe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:22:11 -0300

    Saved ruzwana/main.py at Timestamp: 2019-09-21 15:22:11

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,3 +1,4 @@
 # Laurenice
 from _spy.vittolino.main import Cena, Elemento, Texto
 cenaArvore = "https://ogimg.infoglobo.com.br/in/19343704-906-99e/FT1086A/652/x2015_854741199-img_5130.jpg_20151002.jpg.pagespeed.ic.felWKzcrlc.jpg"
+elementopassaro = "http://ondaverde.org.br/site/wp-content/uploads/2019/06/passaro-solto-background-transparente.png"
