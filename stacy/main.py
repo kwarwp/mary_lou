83ec79d93010f57f86e94b80ec484b36105a2da2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:21:46 -0300

    Saved stacy/main.py at Timestamp: 2019-09-21 15:21:46

--- stacy/main.py
+++ stacy/main.py
@@ -1,4 +1,6 @@
 # Carol
 from _spy.vittolino.main import Cena, Elemento,Texto 
 cena praia= "https://casalnomade.com/wp-content/uploads/2018/11/praia-de-bacutia-1.jpg"
+elemento arvore "https://www.colegioweb.com.br/wp-content/uploads/2012/05/11-300x194.png"
+texto 
 
