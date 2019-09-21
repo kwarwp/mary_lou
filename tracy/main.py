7e7c26fed6908275eaeee46ce8a6ca269b857130
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:20:41 -0300

    Saved tracy/main.py at Timestamp: 2019-09-21 15:20:41

--- tracy/main.py
+++ tracy/main.py
@@ -1,3 +1,4 @@
 # marcelo alves
 from _spy.vittolino.main import Cena, Elemento, Texto
 cenahotel = "https://www.ahstatic.com/photos/3227_ho_00_p_1024x768.jpg"
+elementopassaro = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR91FWFESrb9vtbMzHLNzfeFSkyw9FdMpOMeexC71WS-VgZBp1F"
