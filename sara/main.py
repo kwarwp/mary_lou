156707b7b6394848552cf0c283ea2b526591f5ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:13:42 -0300

    Saved sara/main.py at Timestamp: 2019-09-21 15:13:41

--- sara/main.py
+++ sara/main.py
@@ -1,3 +1,3 @@
 #FABIANO
 from _spy.vittolino.main import Cena, Elemento, Texto
-cenaobiwankenobi
+cenaobi-wankenobi = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAGzRfe.img?h=564&w=624&m=6&q=60&o=f&l=f&x=246&y=140"
