2d9db00a3e7cfdb3fdc8fb5616b2993b224811aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:18:26 -0300

    Saved sara/main.py at Timestamp: 2019-09-21 15:18:25

--- sara/main.py
+++ sara/main.py
@@ -1,3 +1,4 @@
 #FABIANO
 from _spy.vittolino.main import Cena, Elemento, Texto
 cenaobi-wankenobi = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAGzRfe.img?h=564&w=624&m=6&q=60&o=f&l=f&x=246&y=140"
+elemento = "https://vignette.wikia.nocookie.net/starwars/images/c/cd/Battle_Droid.png/revision/latest?cb=20160910192832"
