c3cab81aef52612b26ad5ea397cc8b06400a5928
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:08:06 -0300

    Saved sara/__log__.py at Timestamp: 2019-09-21 15:08:06

--- sara/__log__.py
+++ sara/__log__.py
@@ -6,3 +6,10 @@
                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Sep 21 2019 12:08:05.847 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  cenaobi-wankenobi = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAGzRfe.img?h=564&w=624&m=6&q=60&o=f&l=f&x=246&y=140
+                                                                                                                                   ^
+SyntaxError: EOL while scanning string literal
+'''},
