73ef8bbcec78b798116b22c242ad957019f0605d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:22:53 -0300

    Saved roxanne/main.py at Timestamp: 2019-09-21 15:22:53

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,3 +1,4 @@
 # andre
 from _spy.vittolino.main import Cena, Elemento, Texto
 cenapalacio = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Palacio_Fronteira_-_Lisbon_%287721187114%29.jpg/320px-Palacio_Fronteira_-_Lisbon_%287721187114%29.jpg"
+elementorei = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Heraldic_Royal_Crown_%28Common%29.svg/1200px-Heraldic_Royal_Crown_%28Common%29.svg.png"
