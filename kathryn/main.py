ed03a1a89dedb3214d586bda66edf67719bfc868
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:13:19 -0300

    Saved kathryn/main.py at Timestamp: 2019-09-21 15:13:19

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,2 +1,3 @@
 # cibele
 from _spy.vittolino.main import Cena, Elemento, Texto
+cenacastle = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bodiam-castle-10My8-1197.jpg/300px-Bodiam-castle-10My8-1197.jpg"
