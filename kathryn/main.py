28b810ac07afeccd5f9649affb2767b8abe6f56a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:16:17 -0300

    Saved kathryn/main.py at Timestamp: 2019-09-21 15:16:16

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,3 +1,4 @@
 # cibele
 from _spy.vittolino.main import Cena, Elemento, Texto
 cenacastle = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bodiam-castle-10My8-1197.jpg/300px-Bodiam-castle-10My8-1197.jpg"
+elementofrog = "https://runescape.wiki/images/3/3b/Golden_poison_frog.png?db63b"
