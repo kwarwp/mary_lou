b3f29cdcb52fbe401238881644bdd75b1eecec32
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-21 12:13:24 -0300

    Saved courtney/main.py at Timestamp: 2019-09-21 15:13:24

--- courtney/main.py
+++ courtney/main.py
@@ -1,2 +1,3 @@
 #Barbara
-from_spy.vittilino.import cena,elemento,texto
+from_spy.vittilino.import Cena, Elemento, Texto
+cena = "https://www.google.com.br/url?sa=i&source=images&cd=&ved=2ahUKEwjF59_XmOLkAhUtGbkGHdRuAmEQjRx6BAgBEAQ&url=http%3A%2F%2Fg1.globo.com%2Fturismo-e-viagem%2Fnoticia%2F2013%2F08%2Fcastelo-de-contos-de-fadas-na-baviera-inspirou-parques-da-disney.html&psig=AOvVaw2ZSggFnDpW-mZpO5cMrpNo&ust=1569164837378508"
