514510b0b434e93defcf12c9463bcbd45e7ee174
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:16:56 -0300

    Saved morgan/main.py @2019-10-26 15:16

--- morgan/main.py
+++ morgan/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 cena3 = "https://i.imgur.com/mgp4E0g.png"
 elemento ="encurtador.com.br/aegw0"
 
-def historia():
+def Historia():
 	 cenaHolmes = Cena( img = cena3)
-	cenaHolmes.vai()
-historia()
+	cenaHolmes.vai ()
+Historia()
