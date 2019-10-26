41740da44ab6366f5303fafe0a15d95ac88acf34
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:28:08 -0300

    Saved naomi/main.py @2019-10-26 15:28

--- naomi/main.py
+++ naomi/main.py
@@ -5,5 +5,8 @@ elemento = "encurtador.com.br/aegw0"
 
 def Historia():
 	cenaHolmes = Cena( img = cena2)
+	elem = Elemento(img = elemento, 
+                       tit="Story",
+                       style=dict(left=150, top=60, width=60, hight=200))
 	cenaHolmes.vai()
 Historia()
