6cf6bef3a33a2af877ec9359064fa7d70cceac24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:12:03 -0300

    Saved lisa/main.py @2019-10-26 15:12

--- lisa/main.py
+++ lisa/main.py
@@ -1,5 +1,5 @@
 # larissa
-from _spy.vitollino.main import cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 cena9 = "https://i.imgur.com/LhZvA0Q.png"
 elemento = "encurtador.com.br/aegw0"
 
