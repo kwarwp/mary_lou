69b9383664d1e87116f6bcdfe3bf063db90ddfba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:16:24 -0300

    Saved morgan/__log__.py @2019-10-26 15:16

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -20,3 +20,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Oct 26 2019 12:16:24.13 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  cenaHolmes.vai()
+  ^
+IndentationError: unexpected indent
+'''},
