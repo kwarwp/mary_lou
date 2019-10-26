d433f8fefb55ae8e4f180b3b7236fdf2a31e1a6a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:11:29 -0300

    Created naomi/__log__.py @2019-10-26 15:11

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Oct 26 2019 12:11:27.733 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  cena 2 = "https://i.imgur.com/JcgDwgJ.png"
+        ^
+SyntaxError: invalid syntax
+'''},
