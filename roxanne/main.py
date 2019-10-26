fbeef6d86de993044d391dfa2de33f12bbd5b699
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:10:12 -0300

    Saved roxanne/main.py @2019-10-26 15:10

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,7 +1,7 @@
 # andre
 from _spy.vittolino.main import Cena, Elemento, Texto
 cena11 = "https://i.imgur.com/E7aDjws.png" 
-elemento =
+elemento = "encurtador.com.br/aegw0"
 
 def historia():
 	cenaHolmes = Cena( img = cena11)
