e8fb3ea6de4c7470ed8c2568b22086bd8c92ca8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:11:23 -0300

    Saved morgan/main.py @2019-10-26 15:11

--- morgan/main.py
+++ morgan/main.py
@@ -1,1 +1,9 @@
 # Jane
+from _spy.vitolino.main import cena,elemento,texto
+cena3 = "https://i.imgur.com/mgp4E0g.png"
+elemento ="encurtador.com.br/aegw0"
+
+def historia():
+	 cenaHolmes=Cena(img = cena3)
+	cenaHolmes.vai()
+historia()
