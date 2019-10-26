3b85f336d97b9ad4d3f59e56ddac07f4dc68928c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:08:56 -0300

    Saved roxanne/main.py @2019-10-26 15:08

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,4 +1,9 @@
 # andre
 from _spy.vittolino.main import Cena, Elemento, Texto
-cenapalacio = "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Palacio_Fronteira_-_Lisbon_%287721187114%29.jpg/320px-Palacio_Fronteira_-_Lisbon_%287721187114%29.jpg"
-elementorei = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Heraldic_Royal_Crown_%28Common%29.svg/1200px-Heraldic_Royal_Crown_%28Common%29.svg.png"
+cena11 = "https://i.imgur.com/E7aDjws.png" 
+elemento =
+
+def historia():
+	cenaHolmes = Cena( img = cena11)
+	cenaHolmes.vai()
+Historia()
