57a9198cb7c8584f4ff48f76597af4f1f0d794b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:13:30 -0300

    Created morgan/__log__.py @2019-10-26 15:13

---
+++ morgan/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Oct 26 2019 12:13:26.794 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  cenaHolmes.vai()
+  ^
+IndentationError: unexpected indent
+'''},
