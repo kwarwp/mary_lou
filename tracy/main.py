2515bb9491829d3dd2e4d44d1915ea8b3aeceb3a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:18:14 -0300

    Saved tracy/main.py @2019-10-26 15:18

--- tracy/main.py
+++ tracy/main.py
@@ -1,6 +1,6 @@
 # marcelo alves
 from _spy.vitollino.main import Cena, Elemento, Texto
-cena8="https://i.imgur.com/MyY4aCR.png"
+cena8= "https://i.imgur.com/MyY4aCR.png"
 elemento ="encurtado.com.br/aegw0"
 
 def Historia():
