83a8230eb65ddd86ea05bde4c59740dc2bab6baa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:28:40 -0300

    Saved natalia/main.py @2019-10-26 15:28

--- natalia/main.py
+++ natalia/main.py
@@ -1,3 +1,12 @@
 # Randolph
-from_spy.vittolino.main import Cena, Elemento, Texto
-cenaDigitalplatform = "https://www.osaogoncalo.com.br/geral/58944/plataforma-digital-completa-um-ano-na-engenhoca-e-a-comemoracao-sera-dia-30
+from_spy.vitollino.main import Cena, Elemento, Texto
+cena1 = "http://i.imgur.com/8vImpkP.png"
+elemento ="encurtador.com.br/aegw0"
+
+def historia():
+cenahomes= cena( img = cena11)
+elem = Elemento ( img = elemento,
+                 tit="Story"
+                 style=dict(left=150, top=60, width=60, higth=200))
+cenahomes.vai()
+Historia()
