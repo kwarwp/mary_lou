a7c086a604713bd8380847a578bdd55adf43ef76
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:11:43 -0300

    Saved roxanne/main.py @2019-10-26 15:11

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 cena11 = "https://i.imgur.com/E7aDjws.png" 
 elemento = "encurtador.com.br/aegw0"
 
-def historia():
+def Historia():
 	cenaHolmes = Cena( img = cena11)
 	cenaHolmes.vai()
 Historia()
