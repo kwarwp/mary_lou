1f26871cc65e4ce333fc0a1e21fe45bba6014bf9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:11:20 -0300

    Saved roxanne/main.py @2019-10-26 15:11

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,5 +1,5 @@
 # andre
-from _spy.vittolino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto
 cena11 = "https://i.imgur.com/E7aDjws.png" 
 elemento = "encurtador.com.br/aegw0"
 
