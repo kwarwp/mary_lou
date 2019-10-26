d9ecabc85f818821709ceb3c0a1f0492624c3ef0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:06:05 -0300

    Created lisa/main.py @2019-10-26 15:06

---
+++ lisa/main.py
@@ -0,0 +1,9 @@
+# larissa
+from _spy.vittolino.main import cena, Elemento, Texto
+cena9 = "https://i.imgur.com/LhZvA0Q.png"
+elemento =
+
+def Historia():
+	cenaHolmes = Cena( img = cena9)
+    cenaholmes.vai()
+Historia()
