a9ff3b2d03c100f1946e9c81038101eb191c6b05
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:00:27 -0300

    Saved amanda/__log__.py @2019-10-26 15:00

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -3,3 +3,7 @@
 'error': '''
 Exception: <SyntaxError: Unexpected token '{'>
 '''},
+{'date': 'Sat Oct 26 2019 12:00:26.679 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <SyntaxError: Unexpected token '{'>
+'''},
