7e0c0780a9d38c0028f167b5e2aa721a824a81bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:12:10 -0300

    Created callie/main.py @2019-10-26 15:12

---
+++ callie/main.py
@@ -0,0 +1,9 @@
+# callie
+from_spu.vitollino.main import Cena, Elemento, Texto
+cena7 = "https://i.imgur.com/sXTvNpA.png"
+elemento = "encurtador.com.br/aegw0"
+
+def Historia():
+	cenaHolmes = Cena( img = cena7)
+	cenaHolmes.vai()
+Historia()
