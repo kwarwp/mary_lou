4e109c5fc8ffae3753466c795413861a70581c61
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:10:32 -0300

    Saved lisa/__log__.py @2019-10-26 15:10

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -6,3 +6,10 @@
              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Oct 26 2019 12:10:32.342 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  cenaholmes.vai()
+  ^
+IndentationError: unexpected indent
+'''},
