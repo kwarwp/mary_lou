3324ddca0f908c0cd5bc05166e2d320f3574f91f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:14:46 -0300

    Saved naomi/__log__.py @2019-10-26 15:14

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: invalid syntax
              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Oct 26 2019 12:14:46.174 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  Historia():
+             ^
+SyntaxError: invalid syntax
+'''},
