8fe8562106365a04286cdb837b6b5add3cc9de98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:11:22 -0300

    Created naomi/main.py @2019-10-26 15:11

---
+++ naomi/main.py
@@ -0,0 +1,9 @@
+# joice
+from _spy.vitollino.main import Cena, Elemento, Texto
+cena 2 = "https://i.imgur.com/JcgDwgJ.png"
+elemento = "encurtador.com.br/aegw0"
+
+def Historia():
+	cenahomes = Cena( img = cena2)
+	cenaholmes.vai()
+Historia():
