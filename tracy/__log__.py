ac248e07e8cc34964bb10821427a100f8dd9254a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:17:15 -0300

    Created tracy/__log__.py @2019-10-26 15:17

---
+++ tracy/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Sat Oct 26 2019 12:17:13.425 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    Historia()
+  module <module> line 7
+    	cenaHolmes=cena(img=cena8)
+NameError: name 'cena' is not defined
+'''},
