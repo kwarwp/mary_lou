fb5115b5acc2dc49c6703851efff166f03fd4adc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:05:56 -0300

    Saved kathryn/main.py @2019-10-26 15:05

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,4 +1,9 @@
 # cibele
 from _spy.vittolino.main import Cena, Elemento, Texto
-cenacastle = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Bodiam-castle-10My8-1197.jpg/300px-Bodiam-castle-10My8-1197.jpg"
-elementofrog = "https://runescape.wiki/images/3/3b/Golden_poison_frog.png?db63b"
+cena11 = "https://i.imgur.com/8vImpkP.png"
+elemento = 
+
+def Historia():
+	cenaHolmes = Cena( img = cena11)
+	cenaHolmes.vai()
+Historia()
