1a579c66edc76e2212468a3eb8ffbce32ab84ec8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-26 12:16:20 -0300

    Saved morgan/main.py @2019-10-26 15:16

--- morgan/main.py
+++ morgan/main.py
@@ -4,6 +4,6 @@ cena3 = "https://i.imgur.com/mgp4E0g.png"
 elemento ="encurtador.com.br/aegw0"
 
 def historia():
-	 cenaHolmes=Cena( img = cena3)
+	 cenaHolmes = Cena( img = cena3)
 	cenaHolmes.vai()
 historia()
