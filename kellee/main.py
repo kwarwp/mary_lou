e336a4ef1707786904e1571c0acf87c4c1feb6f4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 10:32:07 -0300

    Saved kellee/main.py @2019-11-09 13:32

--- kellee/main.py
+++ kellee/main.py
@@ -1,7 +1,7 @@
 # Felipe
 from _spy.vittolino.main import Cena, Elemento, Texto 
 cena11 =  "https://i.imgur.com/8vImpkP.png"
-elemento = 
+elemento =  "https: //bit.ly/32y88Mk"
 
 def Historia  ():
 	cenaHolmes = Cena( img = cena11)
