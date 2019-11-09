ae2f06171d8af2985d8de5c4bfced1da97680552
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:38:09 -0300

    Saved soraya/main.py @2019-11-09 14:38

--- soraya/main.py
+++ soraya/main.py
@@ -1,6 +1,6 @@
 # Jacqueline
 from _spy.vitollino.main import Cena, Elemento, Texto
-cena11 = "https://i.imgur.com/8vImpkP.png"
+cena11 = "https://i.imgur.com/VSPdalA.png "
 elemento = "https://bit.ly/32y88Mk"
 
 def Historia():
