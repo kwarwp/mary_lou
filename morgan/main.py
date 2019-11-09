4c29210ab10167e152b0ce89cb358bee4900cb29
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:08:45 -0300

    Saved morgan/main.py @2019-11-09 14:08

--- morgan/main.py
+++ morgan/main.py
@@ -1,12 +1,18 @@
 # Jane
 from _spy.vitollino.main import Cena,Elemento,Texto
 cena3 = "https://i.imgur.com/mgp4E0g.png"
-elemento ="encurtador.com.br/aegw0"
+elemento ="https://bit.ly/32y88Mk"
 
 def Historia():
 	cenaHolmes = Cena( img = cena3)
 	elen = Elemento  (img = elemento,
                        tit="Story",
                        style=dict(left=150, top=60, width=60, hight=200))
+	elem.entra(cenaHolmes)
+	txtElem = Texto (cenaHolmes,
+    			 "
+Holmes is looking at a hat. Deduct who owns it.")
+       elem.vai = txtElem.vai
 	cenaHolmes.vai()
 Historia()
+            
