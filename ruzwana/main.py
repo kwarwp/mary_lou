8fa634587188aacb17624a75efef5e9ecf3ae5ff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:31:58 -0300

    Saved ruzwana/main.py @2019-11-09 14:31

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,4 +1,18 @@
 # Laurenice
-from _spy.vittolino.main import Cena, Elemento, Texto
-cenaArvore = "https://ogimg.infoglobo.com.br/in/19343704-906-99e/FT1086A/652/x2015_854741199-img_5130.jpg_20151002.jpg.pagespeed.ic.felWKzcrlc.jpg"
-elementopassaro = "http://ondaverde.org.br/site/wp-content/uploads/2019/06/passaro-solto-background-transparente.png"
+from _spy.vitollino.main import Cena, Elemento, Texto
+cena11 ="https://i.imgur.com/K1Amaae.png" 
+elemento ="https://bit.ly/32y88Mk" 
+ 
+def Historia() 
+
+	cenaHolmes = ( img = cena11)
+	 elem Elemento = (img = elemento)
+                   tit = "story", 
+                   stile =dict(left=150, top=60, width=60, highit=200) 
+	elem.entra(cenaHolmes)
+	txtElem = Texto (cenaHolmes,
+    			 "")
+	elem.vai = txtElem.vai
+	cenaHolmes.vai()
+Historia()
+
