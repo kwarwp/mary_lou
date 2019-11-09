ff1383a55ff0e2aa0b0703efc9272f994f604b4b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 10:21:31 -0300

    Saved soraya/main.py @2019-11-09 13:21

--- soraya/main.py
+++ soraya/main.py
@@ -1,1 +1,12 @@
 # Jacqueline
+from _spy.vitollino.main import Cena, Elemento, Texto
+cena11 = "https://i.imgur.com/8vImpkP.png"
+elemento = "encurtador.com.br/aegw0"
+
+def Historia():
+	cenaHolmes = Cena( img = cena11)
+	elem = Elemento( img = elemento,
+                     tit="Story",
+                     style=dict(left=150, top=60, width=60, hight=200))
+	cenaHolmes.vai()
+Historia()
