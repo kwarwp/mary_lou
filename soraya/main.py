59d5f0abb036e424bd73652277834ba147e5720f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:40:27 -0300

    Saved soraya/main.py @2019-11-09 14:40

--- soraya/main.py
+++ soraya/main.py
@@ -11,6 +11,6 @@ def Historia():
 	elem.entra(cenaHolmes)
 	txtElem= Texto (cenaHolmes,
 			"The duchess is awaiting news of a possible pregnancy.")
-      elem.vai = txtElem.vai
+	elem.vai = txtElem.vai
 	cenaHolmes.vai()
 Historia()
