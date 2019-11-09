8555704d50a167344513bd038a4ee3f4981f189b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:20:30 -0300

    Saved morgan/main.py @2019-11-09 14:20

--- morgan/main.py
+++ morgan/main.py
@@ -10,9 +10,8 @@ def Historia():
                        style=dict(left=150, top=60, width=60, hight=200))
 	elem.entra(cenaHolmes)
 	txtElem = Texto (cenaHolmes,
-    			 "
-Holmes is looking at a hat. Deduct who owns it.")
-       elem.vai = txtElem.vai
+    			 "Holmes is looking at a hat. Deduct who owns it.")
+	elem.vai = txtElem.vai
 	cenaHolmes.vai()
 Historia()
             
