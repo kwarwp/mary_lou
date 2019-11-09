8110200780e5b017d335b655d363736cd26fcdcb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:34:59 -0300

    Saved ruzwana/main.py @2019-11-09 14:34

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -4,7 +4,7 @@ cena11 ="https://i.imgur.com/K1Amaae.png"
 elemento = "https://bit.ly/32y88Mk" 
  
 def Historia(): 
-	cenaHolmes = Cena( img = cena11)
+	cenaHolmes = Cena(img = cena11)
 	elem = Elemento (img = elemento,
                       tit = "story", 
                       style =dict(left=150, top=60, width=60, highit=200) 
