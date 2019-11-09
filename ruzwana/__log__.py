fc5668e5b6b64aa520d93b2bcfa1b621259d6245
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:32:11 -0300

    Saved ruzwana/__log__.py @2019-11-09 14:32

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -6,3 +6,10 @@
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Nov 09 2019 11:32:11.505 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  cenaHolmes = ( img = cena11)
+                              ^
+SyntaxError: invalid syntax
+'''},
