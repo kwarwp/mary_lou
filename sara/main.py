8b5a735793db03c51863f8ef38102941db98b080
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:28:18 -0300

    Saved sara/main.py @2019-11-09 14:28

--- sara/main.py
+++ sara/main.py
@@ -1,4 +1,16 @@
 #FABIANO
 from _spy.vittolino.main import Cena, Elemento, Texto
-cenaobi-wankenobi = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAGzRfe.img?h=564&w=624&m=6&q=60&o=f&l=f&x=246&y=140"
-elemento = "https://vignette.wikia.nocookie.net/starwars/images/c/cd/Battle_Droid.png/revision/latest?cb=20160910192832"
+cena11 ="https://i.imgur.com/oTxHoHk.png"
+elemento = "https://bit.ly/32y88MK"
+
+def Historia():
+	cenaHolmes = Cena( img = cena11)
+	elem = Elemento( img = elemento,
+                     tit="Story",
+                     style=dict(left=150, top=60, width=60, hight=200))
+	elem.entra(cenaHolmes)
+    	txtElem = Texto (cenaHolmes,
+            			"")
+    elem.vai = txtelem.vai            
+cenaHolmes.vai()
+Historia()
