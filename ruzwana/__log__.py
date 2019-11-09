3059f2bf78caaaa71c28188b1bbe6bec44eae3d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:32:02 -0300

    Created ruzwana/__log__.py @2019-11-09 14:32

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sat Nov 09 2019 11:32:01.0 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  def Historia() 
+                  ^
+SyntaxError: invalid syntax
+'''},
