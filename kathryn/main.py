d521cf54050b8bf3be8e6a6d3b483a5f5088c5f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:08:37 -0300

    Saved kathryn/main.py @2019-11-09 14:08

--- kathryn/main.py
+++ kathryn/main.py
@@ -1,12 +1,16 @@
 # cibele
 from _spy.vitollino.main import Cena, Elemento, Texto
 cena11 = "https://i.imgur.com/8vImpkP.png"
-elemento = "encurtador.com.br/aegw0"
+elemento = "https://bit.ly/32y88Mk"
 
 def Historia():
-	cenaHolmes = Cena( img = cena11)
-	elem = Elemento( img = elemento,
+	cenaHolmes = Cena(img = cena11)
+	elem = Elemento(img = elemento,
                      tit="Story",
                      style=dict(left=150, top=60, width=60, hight=200))
+	elem.entra(cenaHolmes)
+	txtElem = Texto (cenaHolmes,
+    			 "Hello")
+       elem.vai = txtElem.vai          
 	cenaHolmes.vai()
 Historia()
