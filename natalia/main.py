44d71ded228e5c3782ed26f9531a177b67ee1537
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:15:18 -0300

    Saved natalia/main.py @2019-11-09 14:15

--- natalia/main.py
+++ natalia/main.py
@@ -1,12 +1,12 @@
 # Randolph
 from_spy.vitollino.main import Cena, Elemento, Texto
-cena1 = "http://i.imgur.com/8vImpkP.png"
+cena1 = "https://i.imgur.com/t7XI3nx.png"
 elemento ="encurtador.com.br/aegw0"
 
 def historia():
 cenahomes= cena( img = cena11)
 elem = Elemento ( img = elemento,
                  tit="Story"
-                 style=dict(left=150, top=60, width=60, higth=200))
+                 style=dict(left=150, top=60, width=120, higth=200))
 cenahomes.vai()
 Historia()
