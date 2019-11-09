40f6c43536b9e60ad9bda22f446eb1b9b241e64e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:08:44 -0300

    Saved callie/main.py @2019-11-09 14:08

--- callie/main.py
+++ callie/main.py
@@ -1,12 +1,16 @@
 # callie
 from _spy.vitollino.main import Cena, Elemento, Texto
 cena7 = "https://i.imgur.com/sXTvNpA.png"
-elemento = "encurtador.com.br/aegw0"
+elemento = "https://bit.ly/32y88Mk"
 
 def Historia():
 	cenaHolmes = Cena( img = cena7)
 	elem = Elemento (img + elemeneto, 
                       tit="story",
                       style=dict(left=150, top=60, width=60, hight=200))
+	elem.entra(cenaHolmes)
+	txtEleme = Texto (cenaHolmes,
+    			"")
+        eleme.vai = txtElem.vai
 	cenaHolmes.vai()
 Historia()
