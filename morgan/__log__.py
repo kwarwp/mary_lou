29823eca0c26cd6f1534e1316893f379a0cc16dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:20:35 -0300

    Saved morgan/__log__.py @2019-11-09 14:20

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -48,3 +48,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sat Nov 09 2019 11:20:35.37 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    Historia()
+  module <module> line 11
+    	elem.entra(cenaHolmes)
+NameError: name 'elem' is not defined
+'''},
