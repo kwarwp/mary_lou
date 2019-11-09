5fd75614f85405f4149e57d3f7202347ff7c3884
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:09:29 -0300

    Saved tracy/main.py @2019-11-09 14:09

--- tracy/main.py
+++ tracy/main.py
@@ -1,12 +1,16 @@
 # marcelo alves
 from _spy.vitollino.main import Cena, Elemento, Texto
 cena8= "https://i.imgur.com/MyY4aCR.png"
-elemento ="encurtado.com.br/aegw0"
+elemento ="https://bit.ly/32y88Mk"
 
 def Historia():
 	cenaHolmes = Cena(img = cena8)
     elem = Elemento( img = elemento,
                    tit="story",
                    style=dict(left 
+	elem.entra(cenaHolmes)
+	txtElem = Texto (cenaHolmes,
+    			 "")
+       elem.vai = txtElem.vai
 	cenaHolmes.vai()
 Historia()
