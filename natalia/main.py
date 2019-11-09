47706b91a129a723473eb8b1af347e3b031e12a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:23:53 -0300

    Saved natalia/main.py @2019-11-09 14:23

--- natalia/main.py
+++ natalia/main.py
@@ -8,5 +8,9 @@ cenahomes= cena( img = cena11)
 elem = Elemento ( img = elemento,
                  tit="Story"
                  style=dict(left=150, top=60, width=120, higth=200))
+elem.entra(cenaHolmes)
+txtElem = Texto (cenaHolmes,"Hello")
+
+Elem.vai = txtElem.vai
 cenahomes.vai()
 Historia()
