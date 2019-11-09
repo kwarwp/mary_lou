707dc35c341b2899171ffaf4aa6992a0a11aa6cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:32:29 -0300

    Saved ruzwana/__log__.py @2019-11-09 14:32

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                               ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Nov 09 2019 11:32:29.72 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  elem Elemento = (img = elemento)
+  ^
+IndentationError: unexpected indent
+'''},
