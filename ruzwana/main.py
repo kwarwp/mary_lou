3e4f98ae5fa3e2d7ace024fd1292de392b811b09
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-09 11:34:45 -0300

    Saved ruzwana/main.py @2019-11-09 14:34

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,10 +1,9 @@
 # Laurenice
 from _spy.vitollino.main import Cena, Elemento, Texto
 cena11 ="https://i.imgur.com/K1Amaae.png" 
-elemento ="https://bit.ly/32y88Mk" 
+elemento = "https://bit.ly/32y88Mk" 
  
 def Historia(): 
-
 	cenaHolmes = Cena( img = cena11)
 	elem = Elemento (img = elemento,
                       tit = "story", 
