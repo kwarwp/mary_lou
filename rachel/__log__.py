008325fbacbe9d71b9dd7dd9652f768872e9b477
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 15:45:49 -0300

    Created rachel/__log__.py @2019-11-14 18:45

---
+++ rachel/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Nov 14 2019 15:45:48.577 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  https://addons.mozilla.org/pt-BR/firefox/addon/imgur-upload/
+        ^
+SyntaxError: invalid syntax
+'''},
