cfc2149e49efae6caa7fa71272f84ea41c3543d7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 19:17:25 -0300

    Saved rachel/__log__.py @2019-11-14 22:17

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -148,3 +148,10 @@ NameError: name 'lavaArdente' is not defined
                                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Nov 14 2019 19:17:24.956 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 23
+  elementofaustao = Elemento(img = faustao,
+                                                                                                                     ^
+SyntaxError: invalid syntax
+'''},
