8804555d395198b22666066ddbdab72f09a59da0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 19:56:47 -0300

    Saved rachel/__log__.py @2019-11-14 22:56

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -244,3 +244,10 @@ SyntaxError: invalid syntax
                                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Nov 14 2019 19:56:47.71 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 25
+  elementofaustao = Elemento(img = faustao,
+                                                                                                     ^
+SyntaxError: invalid syntax
+'''},
