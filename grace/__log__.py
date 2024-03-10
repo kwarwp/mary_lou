6863235927bfdc49e28aff7fd112f434f30ba5bd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-03-10 11:55:31 -0300

    Saved grace/__log__.py @2024-03-10 14:55

--- grace/__log__.py
+++ grace/__log__.py
@@ -131,3 +131,23 @@ TypeError: unsupported operand type(s) for //: 'str' and 'int'
     divesq.style.width = width // 3  # 100
 TypeError: unsupported operand type(s) for //: 'str' and 'int'
 '''},
+{'date': 'Sun Mar 10 2024 11:55:31.106 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 9
+    cena = Cena(img="http://activufrj.nce.ufrj.br/studio/labase/mirante.jpg?disp=inline&size=G").vai()
+  module _spy.vitollino.main line 1098
+    self._cria_divs(width)
+  module _spy.vitollino.main line 1103
+    divesq.style.width = width // 3  # 100
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+'''},
