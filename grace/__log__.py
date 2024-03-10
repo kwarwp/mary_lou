4fbf2ab1d65b57c42213ad0b305ce45bc2734d80
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-03-10 11:45:03 -0300

    Created grace/__log__.py @2024-03-10 14:45

---
+++ grace/__log__.py
@@ -0,0 +1,17 @@
+
+{'date': 'Sun Mar 10 2024 11:45:01.385 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    STYLE.update(height="500px", width="500px")
+AttributeError: 'module' object has no attribute 'update'
+'''},
